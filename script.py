3e2e2ee53d7acaba4a9c1606c39c54af9583f222
Cedric <cedric.tonyedjon@gmail.com>
2020-09-19 16:42:01 +0200

    started psedo code for interpretation of Markdown headings

---
+++ script.py
@@ -0,0 +1,15 @@
+
+document = []
+
+def process_lines_below(line_gen, parent, level):
+    for line in line_gen:
+        if line is text:
+            parent["children"].add(line)
+        elif line is heading:
+            lower level:
+                next_line = process_lines_below(line_gen, line, line.level)
+
+                if next_line.level = level
+            higher level:
+                return line
+
