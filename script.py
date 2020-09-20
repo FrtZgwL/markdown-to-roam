180b4b2866eec3f1119fbdb9ef626cbc2fe8de2c
Cedric <cedric.tonyedjon@gmail.com>
2020-09-20 16:38:17 +0200

    non-recursive pseudo code

--- script.py
+++ script.py
@@ -1,15 +1,27 @@
 
-document = []
 
-def process_lines_below(line_gen, parent, level):
-    for line in line_gen:
-        if line is text:
-            parent["children"].add(line)
-        elif line is heading:
-            lower level:
-                next_line = process_lines_below(line_gen, line, line.level)
 
-                if next_line.level = level
-            higher level:
-                return line
+current_level = 0
+parents = {
+    0 = parent_0
+    1 = parent_1
+    2 = parent_2 
+    3 = parent_3
+}
+
+for line in line_gen:
+    # Process one line
+    if line is text:
+        parents[current_level].add(line)
+    elif line is heading:
+        if line.level > current_level:
+            # nest under and switch level
+            parents[lcurrent_level].add(line)
+            current_level = line.level
+            parents[line.level] = line
+        elif line.level <= current_level:
+            current_level = line.level - 1
+            parents[current_level].add(line)
+
+
 
